d88322dce1d762582ab16e92c7780b51127cac6e
RomanSukhai <romansukhai@gmail.com>
2022-08-31 20:01:07 +0300

    add

---
+++ src/test/java/ua/lviv/lgs/Project_14/Project14ApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.lviv.lgs.Project_14;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project14ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
